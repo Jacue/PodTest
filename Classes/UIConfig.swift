28b33b65b1e47ac928e85d67711b8a16910aeb7e
youj <282295491@11.com>
2018-11-21 13:55:58 +0800

    first commit

---
+++ Classes/UIConfig.swift
@@ -0,0 +1,38 @@
+//-------------------------------------------------------------------------------------
+// CMB Confidential
+//
+// Copyright (C) 2017 China Merchants Bank Co., Ltd. All rights reserved.
+//
+// No part of this file may be reproduced or transmitted in any form or by any means,
+// electronic, mechanical, photocopying, recording, or otherwise, without prior
+// written permission of China Merchants Bank Co., Ltd.
+//
+//-------------------------------------------------------------------------------------
+//
+//  UIConfig.swift
+//  Nebula
+//
+//  Created by 门军志 on 2017/4/5.
+//  Copyright © 2017年 Dev. All rights reserved.
+//
+
+import Foundation
+
+struct UIConfig {
+    
+    /// Default view corner radius
+    static let cornerRadius: CGFloat = 4.0
+    
+    /// Disable state default alpha value
+    static let disableAlpha: CGFloat = 0.3
+    
+    /// User avatar width
+    static let avatarWidth: CGFloat = 70
+    
+    ///
+    static let padding1: CGFloat = 44
+    
+    ///
+    static let height1: CGFloat = 45
+    
+}
